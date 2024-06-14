b6cf26f787b80f4c3948b4c445bb08864f52b02b
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-14 16:34:50 +0200

    Creata funzione per chiamare tutte le lingue presenti nel database fei film. Creato array per tutte le lingue. Creata variabile reattiva per la lingua scelta.

---
+++ src/components/LanguageSelect.vue
@@ -0,0 +1,33 @@
+<script>
+import { store } from '../store';
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+    methods:{
+        chooseLanguage(lingua){
+            store.languageChoosen = lingua;
+        },
+    },
+    created(){
+        store.getLanguagesApi();
+    },
+    updated(){
+        console.log(23232 ,store.languageChoosen)
+    },
+}
+</script>
+
+<template>
+    <select name="language" id="language">
+        <option v-for="(language,index) in store.arrayLanguages" :key="index" :value="language.english_name" @click="chooseLanguage(language.iso_639_1)" @change="chooseLanguage(language.iso_639_1)">
+            {{ language.english_name }}
+        </option>
+    </select>
+</template>
+
+<style scoped>
+
+</style>
--- src/store.js
+++ src/store.js
@@ -4,8 +4,10 @@ import axios from 'axios';
 export const store = reactive({
     filterFilmsList: '',
     arrayFilteredList: [],
+    arrayLanguages: [],
+    languageChoosen: "",
     GetApiFilmsList(){
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&language=it-IT&query=${this.filterFilmsList}`)
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&language=${this.languageChoosen}&query=${this.filterFilmsList}`)
         .then((response) => {
             // handle success
             console.log(response.data);
@@ -25,6 +27,21 @@ export const store = reactive({
             // handle success
             console.log(response.data);
             this.arrayFilteredList = response.data
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
+    },
+    getLanguagesApi(){
+        axios.get('https://api.themoviedb.org/3/configuration/languages?api_key=e99307154c6dfb0b4750f6603256716d')
+        .then((response) => {
+            // handle success
+            console.log(response.data);
+            this.arrayLanguages = response.data
         })
         .catch(function (error) {
             // handle error
