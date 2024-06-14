8d8d38e82ddbc97c8f16a0d29011ef43a75a7556
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-14 12:27:37 +0200

    Creato AppButton. Popolato nel template con un button.

---
+++ src/components/AppButton.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+
+    },
+    created(){
+
+    }
+}
+</script>
+
+<template>
+    <button>
+        Invia
+    </button>
+</template>
+
+<style scoped>
+
+</style>
