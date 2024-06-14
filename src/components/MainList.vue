a1c6e2bfac44f18fc8a2bcb03e8486593579d261
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-14 15:11:41 +0200

    Creata un ul e attacato componente listSinglweArticle e apllicato un v-for Fatto iterare sul mio array reattivo immagazzinato nello store. Fatto passare tramite props il singolo film.

--- src/components/MainList.vue
+++ src/components/MainList.vue
@@ -1,23 +1,38 @@
 <script>
+import { store } from '../store';
+import ListSingleArticle from './ListSingleArticle.vue';
 export default {
+    components:{
+        ListSingleArticle,
+    },
     data() {
         return {
-            
+            store,
         }
     },
     methods:{
-
     },
     created(){
-
+        
+    },
+    updated(){
+        
     }
 }
 </script>
 
 <template>
-
+    <section>
+        <ul>
+            <ListSingleArticle v-for="film in store.arrayFilteredList.results" :key="film.id"
+            :singleArticle="film"
+            />
+        </ul>
+    </section>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    ul{
+        padding: 2rem;
+    }
 </style>
