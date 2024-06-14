8134c8740176e47ae529ee0f15fe432449df50e6
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-14 15:07:46 +0200

    Messa funzione di chiamata API in store.

--- src/store.js
+++ src/store.js
@@ -1,5 +1,22 @@
-import { reactive } from 'vue'
+import { reactive } from 'vue';
+import axios from 'axios';
 
 export const store = reactive({
     filterFilmsList: '',
+    arrayFilteredList: [],
+    GetApiFilmsList(){
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&language=it-IT&query=${this.filterFilmsList}`)
+        .then((response) => {
+            // handle success
+            console.log(response.data);
+            this.arrayFilteredList = response.data
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
+    },
 })
