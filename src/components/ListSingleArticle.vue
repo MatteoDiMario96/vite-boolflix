fbb3e02e5df92bd7fb51d313215edd7d7994fe20
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-14 15:13:00 +0200

    Importata props singleArticle. Creazione del template per raffigurazione dei dati voluti : Titolo. Titolo Originale. Lingua. Voto.

--- src/components/ListSingleArticle.vue
+++ src/components/ListSingleArticle.vue
@@ -1,5 +1,10 @@
 <script>
 export default {
+    props:{
+        singleArticle:{
+            type: Object,
+        }
+    },
     data() {
         return {
             
@@ -15,9 +20,60 @@ export default {
 </script>
 
 <template>
-
+    <li>
+        <div id="card">
+            <div>
+                <h3>
+                Titolo:
+            </h3>
+            <h2>
+                {{ singleArticle.title }}
+            </h2>
+            </div>
+            <div>
+                <h3>
+                Titolo Originale:
+            </h3>
+            <h2>
+                {{ singleArticle.original_title }}
+            </h2>
+            </div>
+            <div>
+                <h3>
+                Lingua:
+            </h3>
+            <h2>
+                {{ singleArticle.original_language }}
+            </h2>
+            </div>
+            <div>
+            <h3>
+                Voto:
+            </h3>
+            <h2>
+                {{ singleArticle.vote_average }}
+            </h2>
+            </div>
+        </div>
+    </li>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    div#card{
+        display: flex;
+        flex-direction: column;
+        div{
+        display: flex;
+        align-items: center;
+        margin-bottom: .3rem;
+        h2{
+            font-size: 1.2rem;
+            text-decoration: underline;
+        }
+        h3{
+        font-size: 1rem;
+        margin-right: 1rem;
+        }
+    }
+    }
 </style>
