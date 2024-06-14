b8cd57482926ca7aee72311f50e15b96b3a01387
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-14 15:31:27 +0200

    Aggiunta allo store funzione chiamata API per film e serie tv.

--- src/store.js
+++ src/store.js
@@ -19,4 +19,19 @@ export const store = reactive({
             // always executed
         });
     },
+    GetApiSeriesAndFilmsList(){
+        axios.get(`https://api.themoviedb.org/3/search/multi?api_key=e99307154c6dfb0b4750f6603256716d&language=it-IT&query=${this.filterFilmsList}`)
+        .then((response) => {
+            // handle success
+            console.log(response.data);
+            this.arrayFilteredList = response.data
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
+    },
 })
