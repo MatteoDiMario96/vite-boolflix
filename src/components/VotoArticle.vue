d965c4b6db702039a38c7d7a0b6daec8c458aa9f
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-17 14:06:38 +0200

    Creato componente per il voto. Per poterlo gestire meglio.

---
+++ src/components/VotoArticle.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    props:{
+        votoArticle:{
+            type: Object,
+        },
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+
+    },
+    created(){
+
+    }
+}
+</script>
+
+<template>
+    <h3>
+        Voto:
+    </h3>
+    <h2>
+        {{ votoArticle.vote_average }}
+    </h2>
+</template>
+
+<style scoped>
+
+</style>
