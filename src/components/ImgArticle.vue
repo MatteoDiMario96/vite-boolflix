30614cdcc49c832ff6eb89d319e4f22cf2e8363a
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-17 13:56:37 +0200

    Creazione componente immagine copertine film/serie-tv con src dinamico.

---
+++ src/components/ImgArticle.vue
@@ -0,0 +1,29 @@
+<script>
+import { store } from '../store';
+export default {
+    props:{
+        ImgArticle:{
+            type: Object,
+        },
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+    methods:{
+
+    },
+    created(){
+
+    }
+}
+</script>
+
+<template>
+    <img :src="`https://image.tmdb.org/t/p/w185/${ImgArticle.poster_path}`" alt="">
+</template>
+
+<style lang="scss" scoped>
+
+</style>
