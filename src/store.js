2d6aa7f040e7ceed9bd62189491953db5e59e5ab
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-18 12:03:44 +0200

    Messo il .results direttamente dentro l'array risolvendomi cosi il problema.

--- src/store.js
+++ src/store.js
@@ -11,8 +11,8 @@ export const store = reactive({
         axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&language=${this.languageChoosen}&query=${this.filterFilmsList}`)
         .then((response) => {
             // handle success
-            console.log(12212 ,response.data);
-            this.arrayFilmFilteredList = response.data
+            console.log(12212 ,response.data.results);
+            this.arrayFilmFilteredList = response.data.results
         })
         .catch(function (error) {
             // handle error
@@ -24,8 +24,8 @@ export const store = reactive({
         axios.get(`https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=${this.languageChoosen}&query=${this.filterFilmsList}`)
         .then((response) => {
             // handle success
-            console.log(2323 ,response.data);
-            this.arraySerieFilteredList = response.data
+            console.log(2323 ,response.data.results);
+            this.arraySerieFilteredList = response.data.results
         })
         .catch(function (error) {
             // handle error
@@ -39,7 +39,7 @@ export const store = reactive({
         axios.get('https://api.themoviedb.org/3/configuration/languages?api_key=e99307154c6dfb0b4750f6603256716d')
         .then((response) => {
             // handle success
-            console.log(response.data);
+            // console.log(response.data);
             this.arrayLanguages = response.data
         })
         .catch(function (error) {
