ea79a1398c61f4ec0b699c62d6169eb112248629
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-18 13:24:37 +0200

    Cambiato round in ceil perche bisogna arrotondare per eccesso quindi ho pensato fosse più giusto ceil.

--- src/components/VotoArticle.vue
+++ src/components/VotoArticle.vue
@@ -22,28 +22,28 @@ export default {
     <h3>
         Voto:
     </h3>
-    <div v-if="(Math.round(votoArticle.vote_average / 2) === 0)">
+    <div v-if="(Math.ceil(votoArticle.vote_average / 2) === 0)">
         <font-awesome-icon :icon="['fas', 'star-of-life']" />
     </div>
-    <div v-else-if="(Math.round(votoArticle.vote_average / 2) === 1)">
+    <div v-else-if="(Math.ceil(votoArticle.vote_average / 2) === 1)">
         <font-awesome-icon :icon="['fas', 'star']" />
     </div>
-    <div v-else-if="(Math.round(votoArticle.vote_average / 2) === 2)">
-        <font-awesome-icon :icon="['fas', 'star']" />
-        <font-awesome-icon :icon="['fas', 'star']" />
-    </div>
-    <div v-else-if="(Math.round(votoArticle.vote_average / 2) === 3)">
-        <font-awesome-icon :icon="['fas', 'star']" />
+    <div v-else-if="(Math.ceil(votoArticle.vote_average / 2) === 2)">
         <font-awesome-icon :icon="['fas', 'star']" />
         <font-awesome-icon :icon="['fas', 'star']" />
     </div>
-    <div v-else-if="(Math.round(votoArticle.vote_average / 2) === 4)">
-        <font-awesome-icon :icon="['fas', 'star']" />
+    <div v-else-if="(Math.ceil(votoArticle.vote_average / 2) === 3)">
         <font-awesome-icon :icon="['fas', 'star']" />
         <font-awesome-icon :icon="['fas', 'star']" />
         <font-awesome-icon :icon="['fas', 'star']" />
     </div>
-    <div v-else-if="(Math.round(votoArticle.vote_average / 2) === 5)">
+    <div v-else-if="(Math.ceil(votoArticle.vote_average / 2) === 4)">
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+    </div>
+    <div v-else-if="(Math.ceil(votoArticle.vote_average / 2) === 5)">
         <font-awesome-icon :icon="['fas', 'star']" />
         <font-awesome-icon :icon="['fas', 'star']" />
         <font-awesome-icon :icon="['fas', 'star']" />
