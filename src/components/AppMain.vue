1cbd4b372ff3c59abb530ffc8bb6a67d1edbbff3
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-18 16:46:23 +0200

    Stilizazzione main.

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -25,6 +25,12 @@ export default {
 
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@use '../styles/variables' as *;
+    main{
+        height: calc(100% - 90px);
+        background-color: #434343;
+        color: $text_color_hover;
+        overflow-x: scroll;
+    }
 </style>
