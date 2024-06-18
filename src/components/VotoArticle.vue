eee1d0e2c97529837e2e2417cc9e97367cc8b83c
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-18 12:03:05 +0200

    Creato metodo che se il voto è uguale a 1 fa vedere una stella se è 2 fa vedere 2 stelle...

--- src/components/VotoArticle.vue
+++ src/components/VotoArticle.vue
@@ -7,14 +7,13 @@ export default {
     },
     data() {
         return {
-            
+            arraystars: []
         }
     },
     methods:{
-
     },
     created(){
-
+        
     }
 }
 </script>
@@ -23,9 +22,38 @@ export default {
     <h3>
         Voto:
     </h3>
-    <h2>
-        {{ votoArticle.vote_average }}
-    </h2>
+    <div v-if="(Math.round(votoArticle.vote_average / 2) === 0)">
+        <font-awesome-icon :icon="['fas', 'star-of-life']" />
+    </div>
+    <div v-else-if="(Math.round(votoArticle.vote_average / 2) === 1)">
+        <font-awesome-icon :icon="['fas', 'star']" />
+    </div>
+    <div v-else-if="(Math.round(votoArticle.vote_average / 2) === 2)">
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+    </div>
+    <div v-else-if="(Math.round(votoArticle.vote_average / 2) === 3)">
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+    </div>
+    <div v-else-if="(Math.round(votoArticle.vote_average / 2) === 4)">
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+    </div>
+    <div v-else-if="(Math.round(votoArticle.vote_average / 2) === 5)">
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
+    </div>
+    <div v-else>
+        Non ci sono valutazioni.
+    </div>
+    
 </template>
 
 <style scoped>
